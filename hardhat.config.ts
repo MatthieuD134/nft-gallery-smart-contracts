264b0de1aaade0efb3f73e660aaf220a7cab9d96
Matthieu Daulhiac <96766017+MatthieuD134@users.noreply.github.com>
2024-01-30 14:55:52 +0800

    init repository

---
+++ hardhat.config.ts
@@ -0,0 +1,46 @@
+import { config as dotEnvConfig } from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+dotEnvConfig();
+
+// ------------------
+// READ ENV FILE
+// ------------------
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ALCHEMY_SEPOLIA_API_KEY = process.env.ALCHEMY_SEPOLIA_API_KEY || "";
+// -------------------
+
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_SEPOLIA_API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  solidity: {
+    version: "0.8.23",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+};
+
+export default config;
---
+++ lint-staged.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // this will check Typescript files
+  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+
+  // This will lint and format TypeScript and JavaScript files
+  '**/*.(ts|tsx|js|jsx)': (filenames) => [
+    `yarn eslint --fix ${filenames.map((file) => `"${file}"`).join(' ')}`,
+    `yarn prettier --write ${filenames.map((file) => `"${file}"`).join(' ')}`,
+  ],
+
+  // this will Format MarkDown and JSON
+  '**/*.(md|json)': (filenames) =>
+    `yarn prettier --write ${filenames.map((file) => `"${file}"`).join(' ')}`,
+};
